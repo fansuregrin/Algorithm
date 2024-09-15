f6c1d32f61fd6afadb96b7e6b85a604476e8c363
fansuregrin <pwz113436@gmail.com>
2024-09-15 21:05:29 +0800

    Implement insertion sort by comparing and swaping

--- sorting/insertion_sort/insertion_sort.cc
+++ sorting/insertion_sort/insertion_sort.cc
@@ -26,6 +26,23 @@ void insertion_sort(vector<T> &arr, int &cmp_cnt, int &move_cnt) {
     }
 }
 
+template <typename T>
+void insertion_sort2(vector<T> &arr, int &cmp_cnt, int &swap_cnt) {
+    int len = arr.size();
+    cmp_cnt = 0, swap_cnt = 0;
+    for (int i=1; i<len; ++i) {
+        int j = i;
+        for (; j>=1 && arr[j]<arr[j-1]; j-=1) {
+            swap(arr[j], arr[j-1]);
+            ++cmp_cnt;
+            ++swap_cnt;
+        }
+        if (j >= 1) {
+            ++cmp_cnt;
+        }
+    }
+}
+
 template <typename T, typename F>
 void sort_wrapper(vector<T> &arr, F sort_func) {
     cout << "before sort: " << arr << endl;
@@ -34,22 +51,30 @@ void sort_wrapper(vector<T> &arr, F sort_func) {
     cout << "after  sort: " << arr << endl;
     cout << "array size: " << arr.size()
          << ", compare count: " << cmp_cnt
-         << ", move count: " << move_cnt << '\n' << endl;
+         << ", move/swap count: " << move_cnt << '\n' << endl;
 }
 
 int main() {
     vector<int> a1{1, 3, 2, 0, 6, 4, 5};
+    vector<int> a2 = a1;
     sort_wrapper(a1, insertion_sort<int>);
+    sort_wrapper(a2, insertion_sort2<int>);
     
     vector<int> b1{0, 1, 2, 3, 4, 5, 6};
+    vector<int> b2 = b1;
     sort_wrapper(b1, insertion_sort<int>);
+    sort_wrapper(b2, insertion_sort2<int>);
 
     vector<int> c1{6, 5, 4, 3, 2, 1, 0};
+    vector<int> c2 = c1;
     sort_wrapper(c1, insertion_sort<int>);
+    sort_wrapper(c2, insertion_sort2<int>);
 
     vector<algo::Ball> d1{
         {2, algo::RED}, {0, algo::BLUE}, {2, algo::GREEN},
         {1, algo::GREEN}, {3, algo::GREEN}
     };
+    vector<algo::Ball> d2 = d1;
     sort_wrapper(d1, insertion_sort<algo::Ball>);
+    sort_wrapper(d2, insertion_sort2<algo::Ball>);
 }
