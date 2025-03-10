18df32020d193be3ee86ab48167c9913346ea415
fansuregrin <pwz113436@gmail.com>
2025-03-10 10:58:39 +0800

    feat: Reverse Linked List

---
+++ leetcode/p206/ReverseLinkedList.java
@@ -0,0 +1,52 @@
+package leetcode.p206;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        ListNode head = node1;
+
+        System.out.print("before reverse: ");
+        printList(head);
+        head = reverseList(head);
+        System.out.print("after reverse: ");
+        printList(head);
+    }
+
+    private static void printList(ListNode head) {
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur != head) System.out.print("," + cur.val);
+            else System.out.print(cur.val);
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = newHead;
+            newHead = cur;
+            cur = next;
+        }
+        return newHead;
+    }
+}
